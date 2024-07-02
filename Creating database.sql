26504f302d14d02e4f1f639a84d7f861482dfa0a
Uzma Anjum <anjum.uzma1992@gmail.com>
2024-07-02 10:45:45 +0100

    Update Creating database.sql

--- Creating database.sql
+++ Creating database.sql
@@ -4,13 +4,12 @@ USE `Employees`;
 
 
 CREATE TABLE employee_demographics (
-  employee_id INT NOT NULL,
+  employee_id INT NOT NULL PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   age INT,
   gender VARCHAR(10),
-  birth_date DATE,
-  PRIMARY KEY (employee_id)
+  birth_date DATE
 );
 
 CREATE TABLE employee_salary (
