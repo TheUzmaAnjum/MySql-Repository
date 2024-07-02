e9ebf8c21703bf86fa2253a1582ee4af7a959140
Uzma Anjum <anjum.uzma1992@gmail.com>
2024-07-02 10:35:32 +0100

    Create AdvancedCTE.sql

---
+++ AdvancedCTE.sql
@@ -0,0 +1,7 @@
+--  Common Table Expressions (CTE)
+-- A CTE allows you to define a subquery block that can be referenced within the main query. 
+-- Its kind like a query within a query but with a name and a bit more standardized and in a better format.
+
+-- Let's take a look at the basics of writing a CTE:
+-- First, CTEs start using a "With" Keyword. Now, name this CTE 
+-- Then we say as and within the parenthesis we build our required subquery/table 
